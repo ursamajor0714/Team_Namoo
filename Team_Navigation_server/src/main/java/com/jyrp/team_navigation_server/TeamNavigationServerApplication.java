031da5e8b625f74c9e2f7ef3fcb543d703465dce
박준이 <qlcsps2@gmail.com>
2026-08-03 17:32:12 +0900

    Spring Boot 프로젝트 초기 세팅

---
+++ Team_Navigation_server/src/main/java/com/jyrp/team_navigation_server/TeamNavigationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jyrp.team_navigation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNavigationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamNavigationServerApplication.class, args);
+    }
+
+}
---
+++ Team_Navigation_server/src/test/java/com/jyrp/team_navigation_server/TeamNavigationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyrp.team_navigation_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamNavigationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
